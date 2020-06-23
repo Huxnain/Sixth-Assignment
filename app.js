71d99db8f14a4fb4e67bc888a6620d9900ef5f5b
Huxnain <husnainahmed911@gmail.com>
2020-06-23 21:40:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,164 @@
+//********************************* chapter 21 - 25 *********************************
+
+// task 1
+
+// var first = prompt("Enter First Name");
+// var last = prompt("Enter last Name");
+// var fullname = alert("hello "+first+" "+last);
+
+// task 2
+
+// var first = prompt("fav mob model ??");
+// document.write("mobile model : "+first);
+// document.write("<br>string length = "+first.length);
+
+// task 3
+
+// var str = "Pakistan";
+// var n = str.indexOf("n");
+// document.write(n);
+
+// task 4
+
+// var str = "hello world";
+// var n = str.lastIndexOf("l");
+// document.write(n);
+
+// task 5
+
+// var str = "pakistani";
+// var n = str.charAt(str.length-6);
+// document.write(n);
+
+// task 6
+
+// var first = prompt("Enter First Name");
+// var last = prompt("Enter last Name");
+// var fullname = first.concat(" "+last);
+// document.write("hello "+fullname);
+
+// task 7
+
+// var str = "hyderabad";
+// var res = str.replace("hyder", "islam");
+// document.write(res);
+
+// task 8
+
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var res = message.replace("and", "&");
+// document.write(res);
+
+
+// task 9
+
+// var str ="472";
+// var num =472;
+// document.write("string"+str);
+// document.write("<br>number "+num);
+
+// task 10
+
+// var user = prompt("write your name");
+// document.write(""+user.toUpperCase());
+
+// task 11
+
+// var user = prompt("write your name");
+// document.write(""+user.toLowerCase());
+
+
+//***********************************************************************************
+//********************************* chapter 26 - 30 *********************************
+
+// task 1
+
+// var user = prompt("write a positive integer");
+// document.write("<br>Number "+user);
+// document.write("<br>round off value "+Math.round(user));
+// document.write("<br>floor value "+Math.floor(user));
+// document.write("<br>ceil value "+Math.ceil(user));
+
+// task 2
+
+// var user = prompt("write a negative integer");
+// document.write("<br>Number "+user);
+// document.write("<br>round off value "+Math.round(user));
+// document.write("<br>floor value "+Math.floor(user));
+// document.write("<br>ceil value "+Math.ceil(user));
+
+// task 3
+
+// var user = prompt("write a integer");
+// document.write("<br>absolute value "+Math.abs(user));
+
+// task 4
+
+// var n = Math.floor( Math.random() * 6 );
+// document.write(n);
+
+// task 5
+
+// var n = Math.floor( Math.random() * (1,3) );
+// if(n===1){
+// document.write(n+"<br>random coin value: tails");
+// }else if(n===2){
+//     document.write(n+"<br>random coin value: heads");
+// }
+
+// task 6
+
+// var n = Math.floor( Math.random() * (1,100) );
+// document.write(n);
+
+
+//***********************************************************************************
+//********************************* chapter 30 - 34 *********************************
+
+// task 1
+
+// var n = Date();
+// document.write(n);
+
+// task 2
+
+// var d = new Date();
+// var n = d.getMonth();
+
+// if(n==0){
+//     alert("jan");
+//  }else if(n==1){
+//     alert("feb");
+//  }else if(n==2){
+//     alert("mar");
+//  }else if(n==3){
+//     alert("apr");
+//  }else if(n==4){
+//     alert("may");
+//  }else if(n==5){
+//     alert("june");
+//  }else if(n==6){
+//     alert("july");
+//  }else if(n==7){
+//     alert("aug");
+//  }else if(n==8){
+//     alert("sep");
+//  }else if(n==9){
+//     alert("oct");
+//  }else if(n==10){
+//     alert("nov");
+//  }else if(n==11){
+//     alert("dec");
+//  }
+
+// task 3 
+
+
+
+
+
+
+
+
+
+
